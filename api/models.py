754a1fd8bdb5dc6f00e553eb16fd4a12a7a4b4ec
Quentin Clodion <losabit@Noe.localdomain>
2020-07-15 14:55:26 +0000

    Add fields for online mode

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Game(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
+    turn_id = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +13,13 @@ class Player(models.Model):
     id = models.AutoField(primary_key=True)
     pseudo = models.CharField(max_length=20)
     play_on = models.CharField(max_length=100, null=True)
+    health = models.IntegerField(null=True)
+    canon_orientation = models.FloatField(null=True)
+    puissance = models.FloatField(null=True)
+    shoot = models.IntegerField(null=True)
+    pos_x = models.FloatField(null=True)
+    pos_y = models.FloatField(null=True)
+
 
     def __str__(self):
         return self.pseudo
--- api/serializers.py
+++ api/serializers.py
@@ -5,9 +5,9 @@ from .models import Game
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player
-        fields = ('id', 'pseudo', 'play_on')
+        fields = ('id', 'pseudo', 'play_on', 'health', 'canon_orientation', 'puissance', 'shoot', 'pos_x', 'pos_y')
 
 class GameSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Game
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'turn_id')
